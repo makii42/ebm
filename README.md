03088f4327e77a2185533180fee4141216825915
Ralf Fischer <ralf@meet-the-geek.net>
2014-06-12 22:19:25 +0200

    config read properly so far.

--- README.md
+++ README.md
@@ -4,13 +4,20 @@ ebm
 A lightweight, easy to use build monitor for Jenkins.
 
 
-
-
 Requirements
 ============
 
 To get it up and running, you need the following server components:
 
 * Some lightweight webserver (Apache, nginx will do fine)
-* PHP >=5.4 on the server side to proxy calls
+* PHP >=5.4 on the server side for proxy calls
 
+
+How to set it up
+================
+
+
+- Clone it
+- Create ```config/prod.json``` to add your jenkins configuration
+- Set up apache with PHP to point to ```htdocs```
+- DONE!
--- htdocs/status.php
+++ htdocs/status.php
@@ -4,19 +4,54 @@
  * also allows a more grained auth configuration.
  */
 
-require_once __DIR__ . '../vendor/autoload.php';
+use Igorw\Silex\ConfigServiceProvider;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . '../vendor/autoload.php';
 
 $app = new Silex\Application();
-$env = getenv('APP_ENV') ?: 'prod';
+$env = getenv('APP_ENV') ? : 'prod';
 
-$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__."/../config/$env.json"));
+$app->register(new ConfigServiceProvider(__DIR__ . "/../config/$env.json"));
+
 
 $app->get(
-        '/status/{hostLabel}/{jobName}',
-        function ($hostLabel, $jobName) use ($app)
-        {
+    '/status/{hostLabel}/{jobName}',
+    function ($hostLabel, $jobName) use ($app) {
 
+        $hosts = $app['hosts'];
+
+        if (!array_key_exists($hostLabel, $hosts)) {
+            $app->abort(404, 'That host is not real, dude!');
         }
+        $host = $hosts[$hostLabel];
+        $jobs = $app['jobs'];
+        if(!array_key_exists($hostLabel, $jobs)) {
+            $app->abort(404, 'No jobs for this host, dude!');
+        }
+        $jobsByHost = $jobs[$hostLabel];
+
+        return new JsonResponse($jobsByHost);
+    }
 );
+
+
+$app->error(function (\Exception $e, $code) {
+    $response = array('error' => $code);
+    switch ($code) {
+        case 404:
+            $message = $e->getMessage();
+            if(empty($message)) {
+                $message = 'The requested page could not be found.';
+            }
+            $response['message'] = $message;
+            break;
+        default:
+            $response['message'] = 'We are sorry, but something went terribly wrong.';
+    }
+
+    $jsonResponse = new JsonResponse($response);
+    return $jsonResponse;
+});
 
 $app->run();
