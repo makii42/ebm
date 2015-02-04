38ef6d77c5d95e056965dc2706655a6c3a22f847
Ralf Fischer <ralf.fischer@paypal.com>
2015-02-04 18:21:51 +0100

    adds .travis.yml, adapts test to vfs file system.

--- test/php/Ebm/ScriptAppenderTest.php
+++ test/php/Ebm/ScriptAppenderTest.php
@@ -4,21 +4,26 @@ namespace Ebm;
 
 use Monolog\Logger;
 use Symfony\Component\Finder\Finder;
+use VirtualFileSystem\FileSystem;
 
+
+/**
+ * Yeah, I know, this test is testing the symfony finder as well. Send me a PR
+ * if you care to change this.
+ *
+ * @package Ebm
+ */
 class ScriptAppenderTest extends \PHPUnit_Framework_TestCase
 {
 
     /** @var Logger */
     private $loggerMock;
 
-
-
     protected function setUp()
     {
         parent::setUp();
         $this->loggerMock = $this->getMockBuilder('Monolog\Logger')->disableOriginalConstructor()->getMock();
     }
-
 
 
     public function testAppenderWorksWithoutFinder()
@@ -30,12 +35,14 @@ class ScriptAppenderTest extends \PHPUnit_Framework_TestCase
 
 
 
-    public function testAppenderWorksWithOneFinder()
+    public function testAppenderWorksWithOneFinderAndDefaultSeparator()
     {
+        $fs = $this->prepareHelloWorldVfs();
+
         $separator = PHP_EOL . PHP_EOL . PHP_EOL;
 
         $finder = new Finder();
-        $finder->files()->in(__DIR__ . '/../fixtures/appender/oneDir')->name('*txt');
+        $finder->files()->in($fs->path('/dirOne/'))->name('*txt');
 
         $testSubject = new ScriptAppender($this->loggerMock, array($finder));
 
@@ -47,10 +54,11 @@ class ScriptAppenderTest extends \PHPUnit_Framework_TestCase
 
     public function testAppenderWorksWithOneFinderAndCustomSeparator()
     {
+        $fs = $this->prepareHelloWorldVfs();
         $separator = ' ### ';
 
         $finder = new Finder();
-        $finder->files()->in(__DIR__ . '/../fixtures/appender/oneDir')->name('*txt');
+        $finder->files()->in($fs->path('/dirOne/'))->name('*txt');
 
         $testSubject = new ScriptAppender($this->loggerMock, array($finder), $separator);
 
@@ -59,18 +67,36 @@ class ScriptAppenderTest extends \PHPUnit_Framework_TestCase
     }
 
 
+
     public function testAppenderWorksWithMultipleFinders()
     {
+        $fs = $this->prepareHelloWorldVfs();
+        $fs->createDirectory('/another');
+        file_put_contents($fs->path('/another/file.txt'), 'beware of unicorns!');
+
         $separator = PHP_EOL . PHP_EOL . PHP_EOL;
 
         $finderFoo = new Finder();
-        $finderFoo->files()->in(__DIR__ . '/../fixtures/appender/moreDir/foo')->name('*txt');
+        $finderFoo->files()->in($fs->path('/another'))->name('*txt');
         $finderBar = new Finder();
-        $finderBar->files()->in(__DIR__ . '/../fixtures/appender/moreDir/bar')->name('*txt');
+        $finderBar->files()->in($fs->path('/dirOne'))->name('*txt');
 
         $testSubject = new ScriptAppender($this->loggerMock, array($finderFoo, $finderBar));
 
         $this->assertNotEmpty($testSubject->getBlob());
         $this->assertSame('beware of unicorns!' . $separator . 'hello,' . $separator . ' world!', $testSubject->getBlob());
+    }
+
+
+    /**
+     * @return FileSystem
+     */
+    public function prepareHelloWorldVfs()
+    {
+        $fs = new FileSystem();
+        $fs->createDirectory('/dirOne');
+        file_put_contents($fs->path('/dirOne/hello.txt'), 'hello,');
+        file_put_contents($fs->path('/dirOne/world.txt'), ' world!');
+        return $fs;
     }
 }
